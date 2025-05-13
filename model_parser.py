93f249549b71df33a6d7021ad0e06009e4332781
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2025-05-13 15:10:55 +0200

    slightly cleaner ModelParser implementation

--- model_parser.py
+++ model_parser.py
@@ -6,23 +6,58 @@ parameter_pattern = re.compile(r"parameter\s+(\w+)\s+=\s+(\d+\.?\d*(?:[eE]-?\d+)
 derived_pattern = re.compile(r"derived\s+(\w+)\s+=\s+([\w\s\(\)\-\+\*\/\.]*[\w\)])")
 
 
-def get_parsed_parameters(path: str) -> dict[str, float]:
-    results: dict[str, float] = {}
-    with open(path) as file:
-        text = file.read()
-    for match in re.finditer(parameter_pattern, text):
-        name, value = match.group(1, 2)
-        # print(name, float(value))
-        results[name] = float(value)
-    for match in re.finditer(derived_pattern, text):
-        name, value = match.group(1, 2)
-        # don't run this on random model files pls :)
-        derived_value = eval(value, (results | {"sqrt": sqrt, "pi": pi}))
-        print(name, derived_value)
-        results[name] = derived_value
+class ModelParser():
 
-    return results
+    _parameters: dict[str, float]
+    _raw_derived_parameters: dict[str, str]
+
+    def __init__(self, path: str):
+        self._parameters = {}
+        self._raw_derived_parameters = {}
+        with open(path) as file:
+            text = file.read()
+        for match in re.finditer(parameter_pattern, text):
+            name, value = match.group(1, 2)
+            self._parameters[name] = float(value)
+        for match in re.finditer(derived_pattern, text):
+            name, value = match.group(1, 2)
+            self._raw_derived_parameters[name] = value
+        self._update_derived()
+
+
+    def _update_derived(self):
+        for name, value in self._raw_derived_parameters.items():
+            derived_value = eval(value, (self._parameters | {"sqrt": sqrt, "pi": pi}))
+            self._parameters[name] = derived_value
+
+
+    def get_parameters_dict(self) -> dict[str, float]:
+        return self._parameters.copy()
+
+
+    def get_parameters_list(self) -> list[float]:
+        return list(self._parameters.values())
+
+
+    def set_parameter(self, name: str, value: float, update: bool = True):
+        if name in self._parameters:
+            if name in self._raw_derived_parameters:
+                print(f"ERROR: {name} is a derived parameter, calculated as {self._raw_derived_parameters[name]}")
+                raise KeyError
+            self._parameters[name] = value
+        else:
+            raise KeyError
+        if update:
+            self._update_derived()
+
+
+    def set_parameters(self, parameter_dict: dict[str, float]):
+        for name, value in parameter_dict.items():
+            self.set_parameter(name, value, update=False)
+        # we call update manually after all other parameters are set
+        self._update_derived()
 
 
 if __name__  == "__main__":
-    get_parsed_parameters("SM_ac.mdl")
+    model_parser = ModelParser("SM_ac.mdl")
+    print(model_parser.get_parameters_dict())
